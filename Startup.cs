fe56b8ecfdf2bca19f7b9d1a15dbc88c067469fa
Benny van Reeven <bvanreeven@gmail.com>
2017-04-17 11:34:28 +0200

    Rename EmptyWebApplication to NancyApp

--- Startup.cs
+++ Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Nancy.Owin;
 
-namespace EmptyWebApplication
+namespace NancyApp
 {
     public class Startup
     {
